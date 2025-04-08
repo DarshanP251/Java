1e0a83b33882ed185019116496a52bc7c98ef5d4
DarshanP251 <darshanpatil0511@gmail.com>
2025-04-08 12:07:30 +0530

    Saved to main

---
+++ simple.java
@@ -0,0 +1,7 @@
+public class simple{
+    public static void main(String[] args) {
+        System.out.println("Hey! This is a simple sample Java Program.");
+        System.out.println("Inside the Master branch ");
+    }
+
+} 
